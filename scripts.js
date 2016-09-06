e9f9b9ba5a5a9e679eb9f49d69b2ffa0446435ca
Christine Gamble <christine.c.gamble@gmail.com>
2016-09-06 15:02:10 -0600

    random background generation works when random button is clicked

--- scripts.js
+++ scripts.js
@@ -9,3 +9,7 @@ randomBtn.on('click', function() {
 
   $('body').css('background-color', randomColor())
 });
+
+function randomColor() {
+  return 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+}
